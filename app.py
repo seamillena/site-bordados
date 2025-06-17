5fb77cae05a8bd85a798f6df6da9fd7e0fc2fe13
Millena <seamillena@gmail.com>
2025-06-17 19:55:31 -0300

    Configuração para deploy no Render

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'Millen@1801')
 app.config['MYSQL_DB'] = 'bordados_db'
